8c8bcd97b295014247dbc3b6f83bd82727ce8c00
manolo2829 <manudiez2003@gmail.com>
2024-02-23 20:12:51 -0300

    problem

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -3,7 +3,7 @@ function DownloadButton({ canvasRef }) {
 
     const downloadCanvasAsImage = () => {
         const canvas = canvasRef.current;
-        const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        const image = canvas.toDataURL({ format: 'jpeg', quality: 0.8 }).replace("image/png", "image/octet-stream");
         const link = document.createElement('a');
         link.download = 'canvas-image.png';
         link.href = image;
