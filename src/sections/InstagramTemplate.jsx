f5f1b1ead0d5c462945b48e77cf5b9857277fa7a
manolo2829 <manudiez2003@gmail.com>
2024-02-27 18:40:31 -0300

    basics meta

--- src/sections/InstagramTemplate.jsx
+++ src/sections/InstagramTemplate.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components"
 import TemplateManager from "../components/TemplateManager/TemplateManager"
 const formStructure = [
-    { value: '', name: 'condition', x: 71, y: 865, fontSize: 39, label: 'Condición', type: 'text' },
-    { value: '', name: 'location', x: 71, y: 910, fontSize: 33, label: 'Ubicación', type: 'text' },
-    { value: '', name: 'zone', x: 71, y: 945, fontSize: 35, label: 'Departamento', type: 'text' },
-    { value: '', name: 'surface', x: 921, y: 800, fontSize: 23, label: 'Superficie', type: 'text' },
-    { value: '', name: 'garage', x: 921, y: 865, fontSize: 22, label: 'Cochera', type: 'text' },
+    { value: '', name: 'condition', x: 71, y: 865, fontSize: 39, label: 'Condición', placeholder: 'ALQUILER', type: 'text' },
+    { value: '', name: 'location', x: 71, y: 910, fontSize: 33, label: 'Ubicación', placeholder: 'VILLA PALMARES', type: 'text' },
+    { value: '', name: 'zone', x: 71, y: 945, fontSize: 35, label: 'Departamento', placeholder: 'Godoy Cruz - Mendoza', type: 'text' },
+    { value: '', name: 'surface', x: 921, y: 800, fontSize: 23, label: 'Superficie', placeholder: '150', type: 'text' },
+    { value: '', name: 'garage', x: 921, y: 865, fontSize: 22, label: 'Cochera', placeholder: 'Cochera doble', type: 'text' },
     // {value: '', name: 'garage', x: 921, y: 890, fontSize: 22},
-    { value: '', name: 'bed', x: 921, y: 950, fontSize: 22, label: 'Habitaciones', type: 'number' },
-    { value: '', name: 'bath', x: 921, y: 1025, fontSize: 22, label: 'Baños', type: 'number' },
+    { value: '', name: 'bed', x: 921, y: 950, fontSize: 22, label: 'Habitaciones', placeholder: '3', type: 'number' },
+    { value: '', name: 'bath', x: 921, y: 1025, fontSize: 22, label: 'Baños', placeholder: '2', type: 'number' },
 ]
 
 
